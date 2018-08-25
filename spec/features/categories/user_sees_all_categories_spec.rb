953fbf9876f8a3ecc4707cd0377cf6fbed1b0d7d
abdyqew <aqudrat630@gmail,com>
2018-08-25 13:11:53 -0600

    adds feature/test for category index

--- spec/features/categories/user_sees_all_categories_spec.rb
+++ spec/features/categories/user_sees_all_categories_spec.rb
@@ -1,12 +1,16 @@
 require 'rails_helper'
 
 describe "User sees all categories" do
-  xscenario "a user sees all the categories" do
+  scenario "a user sees all the categories" do
+    company = Company.create!(name: "ESPN")
     category = Category.create!(title: "Development")
     category_two = Category.create!(title: "Finance")
+    company.jobs.create!(title: "Developer", level_of_interest: 70, city: "Denver", category_id: category.id)
+    company.jobs.create!(title: "QA Analyst", level_of_interest: 70, city: "New York City", category_id: category.id)
+
 
     visit categories_path
-
+save_and_open_page
     expect(page).to have_content("Development")
   end
 
