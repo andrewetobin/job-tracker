076f5ba2d28fdab423ddb278920b930a627a4a0e
abdyqew <aqudrat630@gmail,com>
2018-08-26 10:55:40 -0600

    Adds contact table/model/test/relationship with company, ATP

---
+++ spec/models/contact_spec.rb
@@ -0,0 +1,43 @@
+require 'rails_helper'
+
+describe Contact do
+ describe "validations" do
+   context "invalid attributes" do
+     it "is invalid without a full_name" do
+       company = Company.create(name: "Turing")
+
+       contact = Contact.new(position: "Recruiter", email: "abc@gmail.com", company_id: company.id)
+
+       expect(contact).to be_invalid
+     end
+     it "is invalid without a postion" do
+       company = Company.create(name: "Turing")
+
+       contact = Contact.new(full_name: "Joseph Davis", email: "abc@gmail.com", company_id: company.id)
+
+       expect(contact).to be_invalid
+     end
+     it "is invalid without an email" do
+       company = Company.create(name: "Turing")
+
+       contact = Contact.new(full_name: "Joseph Davis", position: "Recruiter", company_id: company.id)
+
+       expect(contact).to be_invalid
+     end
+     it "is invalid without a company" do
+       company = Company.create(name: "Turing")
+
+       contact = Contact.new(full_name: "Joseph Davis", position: "Recruiter", email: "abc@gmail.com")
+
+       expect(contact).to be_invalid
+     end
+     it "is valid with attributes" do
+       company = Company.create(name: "Turing")
+
+       contact = company.contacts.create(full_name: "Joseph Davis", position: "Recruiter", email: "abc@gmail.com")
+
+       expect(contact).to be_valid
+     end
+   end
+ end
+end
