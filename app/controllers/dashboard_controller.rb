2573716e2e3912f696dcfb00e65b3c4e6abfbd2e
abdyqew <aqudrat630@gmail,com>
2018-08-27 18:59:50 -0600

    adds count by location controller/view

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,5 +2,6 @@ class DashboardController < ApplicationController
 
   def index
     @count_by_level_of_interest = Job.count_by_level_of_interest
+    @count_by_location = Job.count_by_location
   end
 end
--- spec/features/dashboard/user_sees_dashboard_spec.rb
+++ spec/features/dashboard/user_sees_dashboard_spec.rb
@@ -6,10 +6,10 @@ describe "User visits dashboard" do
     company_1 = Company.create(name: "Turing")
     company_2 = Company.create(name: "Whitewave")
     @job_1 = company_1.jobs.create(title: "Developer", level_of_interest: 40, city: "Denver", category_id: category_1.id)
-    @job_2 = company_1.jobs.create(title: "QA", level_of_interest: 50, city: "Denver", category_id: category_2.id)
-    @job_3 = company_1.jobs.create(title: "Research", level_of_interest: 10, city: "Denver", category_id: category_2.id)
+    @job_2 = company_1.jobs.create(title: "QA", level_of_interest: 50, city: "San Fransisco", category_id: category_2.id)
+    @job_3 = company_1.jobs.create(title: "Research", level_of_interest: 10, city: "San Fransisco", category_id: category_2.id)
     @job_4 = company_2.jobs.create(title: "Dev Ops", level_of_interest: 40, city: "Denver", category_id: category_2.id)
-    @job_5 = company_2.jobs.create(title: "Research", level_of_interest: 30, city: "Denver", category_id: category_1.id)
+    @job_5 = company_2.jobs.create(title: "Research", level_of_interest: 30, city: "New York", category_id: category_1.id)
     @job_6 = company_2.jobs.create(title: "QA", level_of_interest: 50, city: "Denver", category_id: category_1.id)
   end
   it 'should see a dashboard' do
@@ -25,5 +25,14 @@ describe "User visits dashboard" do
 
     expect(page).to have_content("Jobs By Interest")
     expect(page).to have_content("#{@job_1.level_of_interest} has #{count}")
+  end
+  it 'should see count by level of interest' do
+    count = Job.count_by_location[@job_1.city]
+
+    visit '/dashboard'
+save_and_open_page
+    expect(page).to have_content("#{@job_1.city} Jobs")
+    expect(page).to have_content("#{count} Jobs")
+    expect(page).to have_link(@job_1.city)
   end
 end
